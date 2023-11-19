981b08a2b708e44d5a8ce388e7c7ba9bc06fd63d
Vickismity <ibshegs5000@gmail.com>
2023-11-18 18:32:08 -0800

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints digits of number in base 10
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		putchar(n);
+	}
+		putchar('\n');
+
+		return (0);
+}
