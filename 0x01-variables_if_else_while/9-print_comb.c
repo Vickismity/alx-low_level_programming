a4eb9b18aa73c0e4bce9460816409e51d1e19717
Vickismity <ibshegs5000@gmail.com>
2023-11-19 10:43:33 -0800

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	for (n = 48 ; n < 58 ; n++)
+	{
+		putchar (n);
+
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+		putchar('\n');
+
+		return (0);
+}
