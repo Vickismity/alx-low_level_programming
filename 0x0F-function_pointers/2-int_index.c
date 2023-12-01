77fc9b69d620b3e09c42698b74235e14a6111a35
Vickismity <ibshegs5000@gmail.com>
2023-12-01 02:22:35 -0800

    2-int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -19,7 +19,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	for (a = 0 ; a < size ; a++)
 	{
-		if (cmp(array[a] == 1))
+		if (cmp(array[a]))
 			return (a);
 	}
 		return (-1);
