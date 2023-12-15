70d87b0c912580bfd41b57d21e93c53b9bdbc040
Vickismity <ibshegs5000@gmail.com>
2023-12-14 18:18:57 -0800

    1-print_binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_bin - prints the binary representation of a number recursively
+ * @n: decimal number
+ * Return: void
+ */
+void print_bin(unsigned long int n)
+{
+	if (n == 0)
+		return;
+	print_bin(n >> 1);
+	if ((n & 1) == 1)
+		_putchar('1');
+	if ((n & 1) == 0)
+		_putchar('0');
+}
+
+/**
+ * print_binary - function that prints the binary representation of a number
+ * @n: decimal input
+ * Return: 0
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		print_bin(n);
+	}
+}
