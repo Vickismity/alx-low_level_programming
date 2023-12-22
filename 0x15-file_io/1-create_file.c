9eb3434913bc34e325a59a789122e983577ed57d
Vickismity <ibshegs5000@gmail.com>
2023-12-22 14:31:53 -0800

    1-create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,24 +12,19 @@ int create_file(const char *filename, char *text_content)
 	int nl;
 	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
-		text_content = " ";
-
+		text_content = "";
 	for (nl = 0 ; text_content[nl] ; nl++)
 		;
-
 	rwr = write(fd, text_content, nl);
-
 	if (rwr == -1)
 		return (-1);
-
 	close(fd);
-
 	return (1);
 }
